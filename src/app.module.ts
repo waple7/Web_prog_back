bbc95494945b8a62121c7513a5153c913951d396
Waple <naumenko.radmir@mail.ru>
2024-02-13 17:11:05 +0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    ConfigModule.forRoot(),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
