144d1cda208f1b04bf0f8e568914f8311b0fef62
Waple <naumenko.radmir@mail.ru>
2024-02-21 00:30:36 +0300

    lab2 part0

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,6 +6,44 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Render('index')
+  getIndex() {
+    return {
+      title: 'World Nasanmi',
+    };
+  }
+  @Get('index')
+  @Render('index')
+  getIndexMain() {
+    return {
+      title: 'World Nasanmi',
+    };
+  }
+  @Get('services')
+  @Render('services')
+  getServices() {
+    return {
+      title: 'Services',
+    };
+  }
+
+  @Get('team')
+  @Render('team')
+  getTeam() {
+    return {
+      title: 'Team',
+    };
+  }
+  @Get('vacancies')
+  @Render('vacancies')
+  getVacancies() {
+    return {
+      title: 'Vacancies',
+    };
+  }
+  root() {
+    return { message: 'Hello world!' };
+  }
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,23 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as hbs from 'express-handlebars';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(
-    AppModule,
-  );
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+  app.engine(
+    'hbs',
+    hbs.engine({
+      extname: 'hbs',
+      partialsDir: join(__dirname, '..', 'views/partials'),
+      defaultLayout: 'main',
+      layoutsDir: join(__dirname, '..', 'views/layouts'),
+    }),
+  );
   const configService = app.get(ConfigService);
   const port = configService.get('PORT', 2000);
   await app.listen(port);
